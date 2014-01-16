9d9c2233e8838774499813feda0f0bf2c6ba3b92
Thomas Krille <tkrille@gmail.com>
2014-01-16 14:50:04 +0100

    [task] implemented first tests

--- src/main/java/org/osiam/tests/performance/RetrieveListOfUsersWithComplexQuery.java
+++ src/main/java/org/osiam/tests/performance/RetrieveListOfUsersWithComplexQuery.java
@@ -1,27 +1,18 @@
 package org.osiam.tests.performance;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
-import com.github.springtestdbunit.annotation.DatabaseSetup;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("/context.xml")
-@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
-        DbUnitTestExecutionListener.class })
-@DatabaseSetup("/database_seed.xml")
-public class RetrieveListOfUsersWithComplexQuery extends AbstractIntegrationTestBase {
+public class RetrieveListOfUsersWithComplexQuery extends AbstractPerformanceTest {
 
     @Test
-    public void run(){
+    public void run() throws UnsupportedEncodingException {
         String queryString = "filter=meta.created gt \"2011-10-10 00:00:00\""
                 + " and (userName sw \"user\" or userName eq \"irrelevant\")"
                 + " and (email sw \"email\" and email.type eq \"work\")";
-        oConnector.searchUsers(queryString, accessToken);
+
+        osiamConnector.searchUsers(URLEncoder.encode(queryString, "UTF-8"), accessToken);
     }
 }
--- src/main/java/org/osiam/tests/performance/RetrieveListOfUsersWithComplexQueryContainingExtension.java
+++ src/main/java/org/osiam/tests/performance/RetrieveListOfUsersWithComplexQueryContainingExtension.java
@@ -1,29 +1,20 @@
 package org.osiam.tests.performance;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
-import com.github.springtestdbunit.annotation.DatabaseSetup;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("/context.xml")
-@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
-        DbUnitTestExecutionListener.class })
-@DatabaseSetup("/database_seed.xml")
-public class RetrieveListOfUsersWithComplexQueryContainingExtension extends AbstractIntegrationTestBase {
+public class RetrieveListOfUsersWithComplexQueryContainingExtension extends AbstractPerformanceTest {
 
     @Test
-    public void run(){
+    public void run() throws UnsupportedEncodingException {
         String queryString = "filter=meta.created gt \"2011-10-10 00:00:00\""
                 + " and (userName sw \"user\" or userName eq \"irrelevant\")"
                 + " and (email sw \"email\" and email.type eq \"work\")"
                 + " and urn:scim:extension:perfomance.stringValue sq \"Hello\""
                 + " and urn:scim:extension:perfomance.integerValue gt 100";
-        oConnector.searchUsers(queryString, accessToken);
+
+        osiamConnector.searchUsers(URLEncoder.encode(queryString, "UTF-8"), accessToken);
     }
 }
