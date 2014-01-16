df4ba78c37eed1bdc4e66ad758383c5f6bf42ce6
Thomas Krille <tkrille@gmail.com>
2014-01-16 15:14:49 +0100

    [enhancement] improved queries

--- src/main/java/org/osiam/tests/performance/RetrieveListOfUsersWithComplexQuery.java
+++ src/main/java/org/osiam/tests/performance/RetrieveListOfUsersWithComplexQuery.java
@@ -10,8 +10,8 @@ public class RetrieveListOfUsersWithComplexQuery extends AbstractPerformanceTest
     @Test
     public void run() throws UnsupportedEncodingException {
         String queryString = "filter=meta.created gt \"2011-10-10 00:00:00\""
-                + " and (userName sw \"user\" or userName eq \"irrelevant\")"
-                + " and (email sw \"email\" and email.type eq \"work\")";
+                + " and (userName co \"er3\" or userName co \"4\")"
+                + " and (email sw \"email3\" and email.type eq \"work\")";
 
         osiamConnector.searchUsers(URLEncoder.encode(queryString, "UTF-8"), accessToken);
     }
--- src/main/java/org/osiam/tests/performance/RetrieveListOfUsersWithComplexQueryContainingExtension.java
+++ src/main/java/org/osiam/tests/performance/RetrieveListOfUsersWithComplexQueryContainingExtension.java
@@ -10,9 +10,9 @@ public class RetrieveListOfUsersWithComplexQueryContainingExtension extends Abst
     @Test
     public void run() throws UnsupportedEncodingException {
         String queryString = "filter=meta.created gt \"2011-10-10 00:00:00\""
-                + " and (userName sw \"user\" or userName eq \"irrelevant\")"
-                + " and (email sw \"email\" and email.type eq \"work\")"
-                + " and urn:scim:extension:perfomance.stringValue sq \"Hello\""
+                + " and (userName co \"er3\" or userName co \"4\")"
+                + " and (email sw \"email3\" and email.type eq \"work\")"
+                + " and urn:scim:extension:perfomance.stringValue sw \"Hello\""
                 + " and urn:scim:extension:perfomance.integerValue gt 100";
 
         osiamConnector.searchUsers(URLEncoder.encode(queryString, "UTF-8"), accessToken);
